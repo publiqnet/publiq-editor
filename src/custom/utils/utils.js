27075d47ee572bd5d81053fd00a473de4d4701af
sam.alexanyan <sam.alexanyann@gmail.com>
2020-06-24 17:03:03 +0400

    implementation -- input transformation, mostly when input contains image tag.

--- src/custom/utils/utils.js
+++ src/custom/utils/utils.js
@@ -244,3 +244,33 @@ export function renderTexInput( texInput, element ) {
 		return document.createElement( 'p' );// eslint-disable-line
 	}
 }
+
+/**
+ * Converts plain text to its HTML-ized version.
+ *
+ * @param {String} text The plain text to convert.
+ * @returns {String} HTML generated from the plain text.
+ */
+export default function plainTextToHtml( text ) {
+	text = text
+		// Encode <>.
+		.replace( /</g, '&lt;' )
+		.replace( />/g, '&gt;' )
+		// Creates paragraphs for every line breaks.
+		.replace( /\n/g, '</p><p>' )
+		// Preserve trailing spaces (only the first and last one – the rest is handled below).
+		.replace( /^\s/, '&nbsp;' )
+		.replace( /\s$/, '&nbsp;' )
+		// Preserve other subsequent spaces now.
+		.replace( /\s\s/g, ' &nbsp;' );
+
+	if ( text.indexOf( '</p><p>' ) > -1 ) {
+		// If we created paragraphs above, add the trailing ones.
+		text = `<p>${ text }</p>`;
+	}
+
+	// TODO:
+	// * What about '\nfoo' vs ' foo'?
+
+	return text;
+}
